4ac6c6453661de8ddf38854eed2250bef3d2023b
khy <hyunyoung0103@naver.com>
2021-09-06 15:51:57 +0900

    layout

---
+++ src/main/java/com/myHome/myrest/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.myHome.myrest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+}
