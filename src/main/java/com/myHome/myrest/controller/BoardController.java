5683f059bab1f62aa5fe600c54b5da91407e182e
khy <hyunyoung0103@naver.com>
2021-09-07 17:10:15 +0900

    Board build

--- src/main/java/com/myHome/myrest/controller/BoardController.java
+++ src/main/java/com/myHome/myrest/controller/BoardController.java
@@ -1,15 +1,27 @@
 package com.myHome.myrest.controller;
 
+import com.myHome.myrest.model.Board;
+import com.myHome.myrest.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
+    @Autowired
+    private BoardRepository boardRepository;
+
     @GetMapping("/list")
-    public String list() {
+    public String list(Model model) {
+        List<Board> boards = boardRepository.findAll();
+        model.addAttribute("boards", boards);
         return "board/list";
     }
+
 }
---
+++ src/main/java/com/myHome/myrest/repository/BoardRepository.java
@@ -0,0 +1,8 @@
+package com.myHome.myrest.repository;
+
+import com.myHome.myrest.model.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+    
+}
