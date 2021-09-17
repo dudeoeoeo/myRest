7546090d30e4dc6cd0d1467388103e63f2cb0d53
khy <dudekekek@naver.com>
2021-09-17 15:20:22 +0900

    Custom Query library and method

--- src/main/java/com/myHome/myrest/repository/UserRepository.java
+++ src/main/java/com/myHome/myrest/repository/UserRepository.java
@@ -3,14 +3,22 @@ package com.myHome.myrest.repository;
 import com.myHome.myrest.model.User;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.List;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, QuerydslPredicateExecutor<User>, CustomizedUserRepository {
     // 사용자 이름에 맞는 id를 가져옴
     User findByUsername(String username);
 
     // User 클래스의 boards 변수명
     @EntityGraph(attributePaths = { "boards" })
     List<User> findAll();
+
+    @Query("select u from User u where u.username like %?1%")
+    List<User> findByUsernameQuery(String username);
+
+    @Query(value = "select * from User u where u.username like %?1%", nativeQuery = true)
+    List<User> findByUsernameNativeQuery(String username);
 }
